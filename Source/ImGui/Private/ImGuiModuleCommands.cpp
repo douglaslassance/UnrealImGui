933f322af448a2aed5e26523516b6dcf26650f03
Douglas Lassance <douglassance@gmail.com>
2023-01-05 02:12:19 -0800

    Add additional setter commands

--- Source/ImGui/Private/ImGuiModuleCommands.cpp
+++ Source/ImGui/Private/ImGuiModuleCommands.cpp
@@ -7,36 +7,60 @@
 
 
 const TCHAR* const FImGuiModuleCommands::ToggleInput = TEXT("ImGui.ToggleInput");
+const TCHAR* const FImGuiModuleCommands::SetInput = TEXT("ImGui.SetInput");
 const TCHAR* const FImGuiModuleCommands::ToggleKeyboardNavigation = TEXT("ImGui.ToggleKeyboardNavigation");
+const TCHAR* const FImGuiModuleCommands::SetKeyboardNavigation = TEXT("ImGui.SetKeyboardNavigation");
 const TCHAR* const FImGuiModuleCommands::ToggleGamepadNavigation = TEXT("ImGui.ToggleGamepadNavigation");
+const TCHAR* const FImGuiModuleCommands::SetGamepadNavigation = TEXT("ImGui.SetGamepadNavigation");
 const TCHAR* const FImGuiModuleCommands::ToggleKeyboardInputSharing = TEXT("ImGui.ToggleKeyboardInputSharing");
+const TCHAR* const FImGuiModuleCommands::SetKeyboardInputSharing = TEXT("ImGui.SetKeyboardInputSharing");
 const TCHAR* const FImGuiModuleCommands::ToggleGamepadInputSharing = TEXT("ImGui.ToggleGamepadInputSharing");
+const TCHAR* const FImGuiModuleCommands::SetGamepadInputSharing = TEXT("ImGui.SetGamepadInputSharing");
 const TCHAR* const FImGuiModuleCommands::ToggleMouseInputSharing = TEXT("ImGui.ToggleMouseInputSharing");
 const TCHAR* const FImGuiModuleCommands::ToggleDemo = TEXT("ImGui.ToggleDemo");
+const TCHAR* const FImGuiModuleCommands::SetDemo = TEXT("ImGui.SetDemo");
 
 FImGuiModuleCommands::FImGuiModuleCommands(FImGuiModuleProperties& InProperties)
 	: Properties(InProperties)
 	, ToggleInputCommand(ToggleInput,
 		TEXT("Toggle ImGui input mode."),
 		FConsoleCommandDelegate::CreateRaw(this, &FImGuiModuleCommands::ToggleInputImpl))
+	, SetInputCommand(SetInput,
+		TEXT("Set ImGui keyboard navigation."),
+		FConsoleCommandWithArgsDelegate::CreateRaw(this, &FImGuiModuleCommands::SetInputImpl))
 	, ToggleKeyboardNavigationCommand(ToggleKeyboardNavigation,
 		TEXT("Toggle ImGui keyboard navigation."),
 		FConsoleCommandDelegate::CreateRaw(this, &FImGuiModuleCommands::ToggleKeyboardNavigationImpl))
+	, SetKeyboardNavigationCommand(SetKeyboardNavigation,
+		TEXT("Set ImGui keyboard navigation."),
+		FConsoleCommandWithArgsDelegate::CreateRaw(this, &FImGuiModuleCommands::SetKeyboardNavigationImpl))
 	, ToggleGamepadNavigationCommand(ToggleGamepadNavigation,
 		TEXT("Toggle ImGui gamepad navigation."),
 		FConsoleCommandDelegate::CreateRaw(this, &FImGuiModuleCommands::ToggleGamepadNavigationImpl))
+	, SetGamepadNavigationCommand(SetGamepadNavigation,
+		TEXT("Set ImGui gamepad navigation."),
+		FConsoleCommandWithArgsDelegate::CreateRaw(this, &FImGuiModuleCommands::SetGamepadNavigationImpl))
 	, ToggleKeyboardInputSharingCommand(ToggleKeyboardInputSharing,
 		TEXT("Toggle ImGui keyboard input sharing."),
 		FConsoleCommandDelegate::CreateRaw(this, &FImGuiModuleCommands::ToggleKeyboardInputSharingImpl))
+	, SetKeyboardInputSharingCommand(SetKeyboardInputSharing,
+		TEXT("Set ImGui keyboard input sharing."),
+		FConsoleCommandWithArgsDelegate::CreateRaw(this, &FImGuiModuleCommands::SetKeyboardInputSharingImpl))
 	, ToggleGamepadInputSharingCommand(ToggleGamepadInputSharing,
 		TEXT("Toggle ImGui gamepad input sharing."),
 		FConsoleCommandDelegate::CreateRaw(this, &FImGuiModuleCommands::ToggleGamepadInputSharingImpl))
+	, SetGamepadInputSharingCommand(SetGamepadInputSharing,
+		TEXT("Set ImGui gamepad input sharing."),
+		FConsoleCommandWithArgsDelegate::CreateRaw(this, &FImGuiModuleCommands::SetGamepadInputSharingImpl))
 	, ToggleMouseInputSharingCommand(ToggleMouseInputSharing,
 		TEXT("Toggle ImGui mouse input sharing."),
 		FConsoleCommandDelegate::CreateRaw(this, &FImGuiModuleCommands::ToggleMouseInputSharingImpl))
 	, ToggleDemoCommand(ToggleDemo,
 		TEXT("Toggle ImGui demo."),
 		FConsoleCommandDelegate::CreateRaw(this, &FImGuiModuleCommands::ToggleDemoImpl))
+	, SetDemoCommand(SetDemo,
+		TEXT("Set ImGui mouse input sharing."),
+		FConsoleCommandWithArgsDelegate::CreateRaw(this, &FImGuiModuleCommands::SetDemoImpl))
 {
 }
 
@@ -50,9 +74,29 @@ void FImGuiModuleCommands::ToggleInputImpl()
 	Properties.ToggleInput();
 }
 
+void FImGuiModuleCommands::SetInputImpl(const TArray<FString>& Args)
+{
+	bool bIsEnabled = false;
+	if (Args.Num() > 0)
+	{
+		LexFromString(bIsEnabled, *Args[0]);
+	}
+	Properties.SetInputEnabled(bIsEnabled);
+}
+
 void FImGuiModuleCommands::ToggleKeyboardNavigationImpl()
 {
 	Properties.ToggleKeyboardNavigation();
+}
+
+void FImGuiModuleCommands::SetKeyboardNavigationImpl(const TArray<FString>& Args)
+{
+	bool bIsEnabled = false;
+	if (Args.Num() > 0)
+	{
+		LexFromString(bIsEnabled, *Args[0]);
+	}
+	Properties.SetKeyboardNavigationEnabled(bIsEnabled);
 }
 
 void FImGuiModuleCommands::ToggleGamepadNavigationImpl()
@@ -60,14 +104,44 @@ void FImGuiModuleCommands::ToggleGamepadNavigationImpl()
 	Properties.ToggleGamepadNavigation();
 }
 
+void FImGuiModuleCommands::SetGamepadNavigationImpl(const TArray<FString>& Args)
+{
+	bool bIsEnabled = false;
+	if (Args.Num() > 0)
+	{
+		LexFromString(bIsEnabled, *Args[0]);
+	}
+	Properties.SetGamepadNavigationEnabled(bIsEnabled);
+}
+
 void FImGuiModuleCommands::ToggleKeyboardInputSharingImpl()
 {
 	Properties.ToggleKeyboardInputSharing();
 }
 
+void FImGuiModuleCommands::SetKeyboardInputSharingImpl(const TArray<FString>& Args)
+{
+	bool bIsEnabled = false;
+	if (Args.Num() > 0)
+	{
+		LexFromString(bIsEnabled, *Args[0]);
+	}
+	Properties.SetKeyboardInputShared(bIsEnabled);
+}
+
 void FImGuiModuleCommands::ToggleGamepadInputSharingImpl()
 {
 	Properties.ToggleGamepadInputSharing();
+}
+
+void FImGuiModuleCommands::SetGamepadInputSharingImpl(const TArray<FString>& Args)
+{
+	bool bIsEnabled = false;
+	if (Args.Num() > 0)
+	{
+		LexFromString(bIsEnabled, *Args[0]);
+	}
+	Properties.SetGamepadInputShared(bIsEnabled);
 }
 
 void FImGuiModuleCommands::ToggleMouseInputSharingImpl()
@@ -78,4 +152,14 @@ void FImGuiModuleCommands::ToggleMouseInputSharingImpl()
 void FImGuiModuleCommands::ToggleDemoImpl()
 {
 	Properties.ToggleDemo();
+}
+
+void FImGuiModuleCommands::SetDemoImpl(const TArray<FString>& Args)
+{
+	bool bIsEnabled = false;
+	if (Args.Num() > 0)
+	{
+		LexFromString(bIsEnabled, *Args[0]);
+	}
+	Properties.SetShowDemo(bIsEnabled);
 }
--- Source/ImGui/Private/ImGuiModuleCommands.h
+++ Source/ImGui/Private/ImGuiModuleCommands.h
@@ -14,12 +14,18 @@ class FImGuiModuleCommands
 public:
 
 	static const TCHAR* const ToggleInput;
+	static const TCHAR* const SetInput;
 	static const TCHAR* const ToggleKeyboardNavigation;
+	static const TCHAR* const SetKeyboardNavigation;
 	static const TCHAR* const ToggleGamepadNavigation;
+	static const TCHAR* const SetGamepadNavigation;
 	static const TCHAR* const ToggleKeyboardInputSharing;
+	static const TCHAR* const SetKeyboardInputSharing;
 	static const TCHAR* const ToggleGamepadInputSharing;
+	static const TCHAR* const SetGamepadInputSharing;
 	static const TCHAR* const ToggleMouseInputSharing;
 	static const TCHAR* const ToggleDemo;
+	static const TCHAR* const SetDemo;
 
 	FImGuiModuleCommands(FImGuiModuleProperties& InProperties);
 
@@ -28,20 +34,32 @@ public:
 private:
 
 	void ToggleInputImpl();
+	void SetInputImpl(const TArray<FString>& Args);
 	void ToggleKeyboardNavigationImpl();
+	void SetKeyboardNavigationImpl(const TArray< FString >& Args);
 	void ToggleGamepadNavigationImpl();
+	void SetGamepadNavigationImpl(const TArray< FString >& Args);
 	void ToggleKeyboardInputSharingImpl();
+	void SetKeyboardInputSharingImpl(const TArray< FString >& Args);
 	void ToggleGamepadInputSharingImpl();
+	void SetGamepadInputSharingImpl(const TArray< FString >& Args);
 	void ToggleMouseInputSharingImpl();
 	void ToggleDemoImpl();
+	void SetDemoImpl(const TArray< FString >& Args);
 
 	FImGuiModuleProperties& Properties;
 
 	FAutoConsoleCommand ToggleInputCommand;
+	FAutoConsoleCommand SetInputCommand;
 	FAutoConsoleCommand ToggleKeyboardNavigationCommand;
+	FAutoConsoleCommand SetKeyboardNavigationCommand;
 	FAutoConsoleCommand ToggleGamepadNavigationCommand;
+	FAutoConsoleCommand SetGamepadNavigationCommand;
 	FAutoConsoleCommand ToggleKeyboardInputSharingCommand;
+	FAutoConsoleCommand SetKeyboardInputSharingCommand;
 	FAutoConsoleCommand ToggleGamepadInputSharingCommand;
+	FAutoConsoleCommand SetGamepadInputSharingCommand;
 	FAutoConsoleCommand ToggleMouseInputSharingCommand;
 	FAutoConsoleCommand ToggleDemoCommand;
+	FAutoConsoleCommand SetDemoCommand;
 };
